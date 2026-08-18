84a8c3e0043d172bb16b32a40d62c78711dcbdb8
blackbeagle <dgnkartal@yahoo.com>
2026-08-18 22:57:34 +0300

    Temel ve Dashboard sekmeleri için başlangıç yapısı

---
+++ HisseTarama/View Controllers/DashboardViewController.swift
@@ -0,0 +1,8 @@
+import Cocoa
+
+final class DashboardViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ HisseTarama/View Controllers/FundamentalsViewController.swift
@@ -0,0 +1,8 @@
+import Cocoa
+
+final class FundamentalsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
